331f50d47e4adc18323b50b987cef023c77a3c82
Alex <supamumas@gmail.com>
2021-02-18 00:02:05 +0100

    Added library program.

---
+++ arrayLists/src/librarypack/Biblioteca.java
@@ -0,0 +1,128 @@
+package librarypack;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+	private int librosTotal;
+	private ArrayList <Libro> libros;
+	private int cont;
+	
+	public Biblioteca(int n){
+	libros = new ArrayList<Libro>();
+	this.librosTotal = n;
+	this.cont = 0;
+	}
+	
+	public Biblioteca() {
+		this.libros = new ArrayList<Libro>();
+		this.librosTotal = 10;
+		this.cont = 0;
+	}
+	
+	public Biblioteca(int librosTotal, ArrayList<Libro> libros, int cont) {
+		super();
+		this.librosTotal = librosTotal;
+		this.libros = libros;
+		this.cont = cont;
+	}
+	
+	public int getLibrosTotal() {
+		return librosTotal;
+	}
+	public void setLibrosTotal(int librosTotal) {
+		this.librosTotal = librosTotal;
+	}
+	public ArrayList<Libro> getLibros() {
+		return libros;
+	}
+	public void setLibros(ArrayList<Libro> libros) {
+		this.libros = libros;
+	}
+	public int getcont() {
+		return cont;
+	}
+	public void setcont(int cont) {
+		this.cont = cont;
+	}
+	
+	public boolean equals(Biblioteca biblioteca){
+		int num = 0;
+		for (int i = 0; i<biblioteca.libros.size();i++) {
+			for (int j = 0; i<biblioteca.libros.size();i++ ) {
+				if (biblioteca.libros.get(i).equals(libros.get(j+1))) { 
+					num++;
+				}
+			} 
+		}
+		if (num>0)
+		return true;
+		else return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "Biblioteca [librosTotal=" + librosTotal + ", libros=" + libros + ", cont=" + cont + "]";
+	}
+	
+	public boolean buscar(long ISBN) {
+		for (int i = 0; i<libros.size();i++) {
+			if (ISBN == libros.get(i).getISBN()){
+				System.out.println("El libro con este ISBN ya esta en la biblioteca.");
+				System.out.printf("Posicion, %d",this.buscarPos(ISBN));
+				return true;}
+			}
+		System.out.println("El libro con este ISBN no esta en la biblioteca.");
+		return false;
+		}
+		
+		/* Dado un ISBN de un libro, devuelve la posici�n donde est�
+	dentro del array, si no -1*/
+	//GOOD
+	private int buscarPos(long ISBN){
+		int j = -1;
+		int found = -1;
+	for (int i = 0; i < libros.size(); i++){
+		if (ISBN == libros.get(i).getISBN()){
+		found = i;}}
+		if (found != -1) {
+			return found;
+		} else
+		return j;
+		}
+	
+	public boolean insertar(Libro libro){
+		int check = buscarPos(libro.getISBN());
+		if (check >= 0) {
+			System.out.println("Ya hay otro libro igual");
+			return false;
+		} else if (cont>9) {
+			System.out.println("No hay espacio en la biblioteca.");
+			return false;
+		}
+		else { 
+			libros.add(libro);
+			System.out.printf("Libro insertado correctamente, hay %d libros en la biblioteca.",libros.size());
+			return true;
+		}
+	}
+	
+	//GOOD
+	public boolean eliminar(long ISBN){
+		int i = this.buscarPos(ISBN);
+		if (i >= 0) {libros.remove(i);
+			System.out.println("Libro eliminado.");
+			return true;}
+		else { 
+				System.out.println("Libro no encontrado.");
+				return false;}
+		}
+	
+	public void mostrar() {
+		for(Libro pos: libros) {
+			System.out.println(pos.toString());
+		}
+		if (libros.size() == 0) {
+			System.out.println("La agenda esta vacia.");
+		}
+	}
+	}
---
+++ arrayLists/src/librarypack/Main.java
@@ -0,0 +1,83 @@
+package librarypack;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int opcion;
+		Scanner sc = new Scanner(System.in);
+		Biblioteca biblioteca = new Biblioteca();
+		long isbn;
+		String titulo, nombre, apellidos, nacionalidad;
+		int pags, dni, edad;
+		do{
+		System.out.println("1. Insertar un libro");
+		System.out.println("2. Eliminar un libro");
+		System.out.println("3. Buscar un libro");
+		System.out.println("4. Mostrar todos los libros");
+		System.out.println("0. Salir de la aplicacion");
+		System.out.print("Elige opcion: ");
+		opcion = sc.nextInt();
+		sc.nextLine(); //limpiamos el buffer
+		switch(opcion){
+		case 1:
+		
+		System.out.println("Inserta el ISBN:");
+		isbn = sc.nextLong();
+		System.out.println("Inserta el titulo:");
+		titulo = sc.next();
+		System.out.println("Inserta el dni: ");
+		dni = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Inserta el nombre: ");
+		nombre = sc.nextLine();
+		System.out.println("Inserta los apellidos: ");
+		apellidos = sc.nextLine();
+		System.out.println("Inserta la edad");
+		edad = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Inserta la nacionalidad: ");
+		nacionalidad = sc.nextLine();
+		System.out.println("Inserta las paginas:");
+		pags = sc.nextInt();
+		Persona p = new Persona(dni,nombre,apellidos,edad,nacionalidad);
+		Libro libro = new Libro(isbn,titulo,p,pags);
+		biblioteca.insertar(libro);
+		//* Insertar los datos anteriormente
+		/*tecleados en el libro de modo que habr� que
+		decir SI EL LIBRO SE HA INSERTADO
+		CORRECTAMENTE o no en cuyo caso lo
+		normal es que este la biblioteca LLENA y
+		no se haya podido meter mas libros.*/
+		break;
+		case 2:
+			System.out.println("Inserta el ISBN del libro que quieras eliminar.");
+			isbn = sc.nextLong();
+			biblioteca.eliminar(isbn);
+		break;
+		/* Dado un ISBN (que habr� que leer por
+		teclado), buscar dicho libro de la biblioteca.
+		Puede suceder de que ese libro no est� con
+		lo cual habr�a que decir que dicho libro NO
+		ESTA EN LA BIBLIOTECA.*/
+		case 3:
+		System.out.println("Inserta un ISBN:");
+		isbn = sc.nextLong();
+		biblioteca.buscar(isbn);
+		break;
+		case 4:
+		//* Mostrar todos los libros de la biblioteca
+			biblioteca.mostrar();
+		break;
+		case 0:
+		System.out.println("Gracias por usar mi biblioteca");
+		 break;
+		default:
+		System.out.println("Opcion incorrecta");
+		}
+		}while(opcion != 0);
+		sc.close();
+	}
+	
+}
