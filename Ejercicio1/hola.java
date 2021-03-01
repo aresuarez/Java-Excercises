b98ec66b1d546ade8d2b1166bd5dcf119395fb1b
Alex <supamumas@gmail.com>
2021-03-01 14:55:58 +0100

    Update hola.java

--- Ejercicio1/hola.java
+++ Ejercicio1/hola.java
@@ -1,5 +1,5 @@
-public class ASBhola { 
+public class Hola { 
  public static void main (String [ ] args) {
-        System.out.println ("Empezamos la ejecución del programa");
+        System.out.println ("Empezamos la ejecuciÃ³n del programa");
     }
 } 
